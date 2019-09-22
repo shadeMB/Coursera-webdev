a8e51f8c030687df07ce4b1d1cafb469da2b6974
Bogdan Murarasu <murarasu.bogdan@yahoo.com>
2019-09-22 23:16:59 +0300

    Dinamically adding the main content

--- module5-work/restaurantSite/js/script.js
+++ module5-work/restaurantSite/js/script.js
@@ -9,3 +9,36 @@ $(function () {
         }
     });
 });
+
+(function (global) {
+    var dc = {};
+    var homeHtml = "snippets/home-snippet.html";
+
+    // Convenience function for inserting innerHTML for 'select'
+    var insertHtml = function (selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+
+    // Show loading icon inside element identified by 'selector'
+    var showLoading = function (selector) {
+        var html = "<div class='text-center'>";
+        html += "<img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector,html);
+    };
+
+    // On page load (before images or CSS)
+    document.addEventListener("DOMContentLoaded", function (event) {
+        
+        // On first load, show home view
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            homeHtml, function (responseText) {
+                console.log(responseText);
+                document.querySelector("#main-content").innerHTML = responseText.response;
+            }, false
+        );
+    });
+
+    global.$dc = dc;
+})(window);
